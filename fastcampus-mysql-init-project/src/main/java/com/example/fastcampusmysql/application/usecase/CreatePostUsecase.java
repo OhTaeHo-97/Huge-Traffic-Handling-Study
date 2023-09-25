c90b8ea38f03679a3b8611f61198576382a1db99
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-09-25 18:59:06 +0900

    :sparkles: feat: add comments about transaction and apply @Transactional annotation for some methods that need transaction

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/usecase/CreatePostUsecase.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/usecase/CreatePostUsecase.java
@@ -7,6 +7,7 @@ import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import com.example.fastcampusmysql.domain.post.service.TimelineWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +16,22 @@ public class CreatePostUsecase {
     final private FollowReadService followReadService;
     final private TimelineWriteService timelineWriteService;
 
+//    @Transactional
     public Long execute(PostCommand postCommand) {
+        // insert 쿼리가 postWriteService.create()에서 한 번, timelineWriteService.deliveryToTimeline()에서 한 번 나가게 됨
+        //  -> 트랜잭션 필요!
+        //  -> 여기에 @Transactional을 걸어주면 트랜잭션이 묶이면서 연산들이 All or Nothing 연산이 된다!(Atomic 연산이 보장된다)
+        // 여기에 트랜잭션을 걸 것인지는 고민해봐야 하는 문제
+        //  -> 게시물 하나를 작성하는 것에 팔로워가 몇 만명 이상이라면 이 트랜잭션이 너무 길어질 것임
+        //
         var postId = postWriteService.create(postCommand);
+
         var followerMemberIds = followReadService
                 .getFollows(postCommand.memberId())
                 .stream()
                 .map(Follow :: getFromMemberId)
                 .toList();
+
         timelineWriteService.deliveryToTimeline(postId, followerMemberIds);
 
         return postId;
--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -7,6 +7,8 @@ import com.example.fastcampusmysql.domain.member.repository.MemberNicknameHistor
 import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +16,7 @@ public class MemberWriteService {
     final private MemberRepository memberRepository;
     final private MemberNicknameHistoryRepository memberNicknameHistoryRepository;
 
+    @Transactional
     public Member register(RegisterMemberCommand command) {
         /*
             목표 - 회원정보(이메일, 닉네임, 생년월일)를 등록한다
@@ -29,11 +32,31 @@ public class MemberWriteService {
                 .birthday(command.birthday())
                 .build();
 
-        var savedMember = memberRepository.save(member);
-        saveMemberNicknameHistory(savedMember);
+        // 별도의 트랜잭션을 설정하지 않았기 때문에 SQL문 하나하나가 트랜잭션이라고 생각해도 무방하다!
+        //  -> 결국 두 트랜잭션이 별개다!
+        // 회원을 저장하고 이름 변경 내역을 저장하기 직전에 예외를 던진다면 트랜잭션이 적용되지 않아 회원만 저장되고 이름이 저장되지 않는다!
+        // JdbcTemplate을 이용해 트랜잭션을 제어하는 방법은 크게 2가지가 있음
+        //  1. @Transactional 어노테이션을 이용해 선언적으로 트랜잭션을 제어하는 방법
+        //      - 내가 트랜잭션을 보장하고 싶은 메서드 위에다가 @Transactional 어노테이션을 붙이면 SQL은 해당 메서드에서 트랜잭션을 시작하고 해당 메서드가 끝이 날 때 트랜잭션을 commit한다
+        //  2. TransactionTemplate을 사용하여 직접 트랜잭션을 제어하는 방법
+        // Spring을 사용할 때는 선언적 어노테이션을 선호한다
+        //  - 비즈니스 로직에 DB와 종속된 코드들을 숨길 수 있기 때문!
+        // JdbcTemplate을 사용하고 있는데 TransactionTemplate 도움 없이 트랜잭션을 구현하면 코드가 복잡해지고 처리해야 할 것들이 많음
+        //  -> 그래서 대부분 TransactionTemplate이나 @Transactional 어노테이션을 많이 사용!
+        //  -> TransactionTemplate도 많이 사용하지 않음
+
+        // execute()를 통해 트랜잭션 범위를 정해줄 수 있음
+        //  - execute() 안에 콜백 함수로 받음
+        //  - execute() 안에 받은 인자의 범위가 트랜잭션의 범위가 된다
+        TransactionTemplate transactionTemplate = new TransactionTemplate();
+
+        var savedMember = memberRepository.save(member); // 회원을 저장하는 insert 쿼리가 하나 발생할 것임
+
+        saveMemberNicknameHistory(savedMember); // 회원의 이름 변경 내역 저장하는 쿼리가 발생할 것임
         return savedMember;
     }
 
+    @Transactional
     public void changeNickname(Long memberId, String nickname) {
         /*
             1. 회원의 이름을 변경
