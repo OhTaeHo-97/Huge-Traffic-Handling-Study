7d485116925e3df8b1f7b10c516ad1943a1ceb1a
otaeho <dhxogh123123@naver.com>
2023-10-03 23:42:55 +0900

    :sparkles: feat: make new api for adding PostLike

--- fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
+++ fastcampus-mysql-init-project/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.example.fastcampusmysql.application.controller;
 
+import com.example.fastcampusmysql.application.usecase.CreatePostLikeUsecase;
 import com.example.fastcampusmysql.application.usecase.CreatePostUsecase;
 import com.example.fastcampusmysql.application.usecase.GetTimelinePostUsecase;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.dto.PostDto;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import com.example.fastcampusmysql.domain.post.service.PostReadService;
 import com.example.fastcampusmysql.domain.post.service.PostWriteService;
@@ -26,6 +28,7 @@ public class PostController {
     final private PostReadService postReadService;
     final private GetTimelinePostUsecase getTimelinePostUsecase;
     final private CreatePostUsecase createPostUsecase;
+    final private CreatePostLikeUsecase createPostLikeUsecase;
 
     @PostMapping("")
     public Long create(PostCommand command) {
@@ -47,7 +50,7 @@ public class PostController {
 //    }
 
     @GetMapping("/members/{memberId}")
-    public Page<Post> getPosts(
+    public Page<PostDto> getPosts(
             @PathVariable Long memberId,
             Pageable pageable
     ) {
@@ -84,8 +87,15 @@ public class PostController {
 //    }
 
     // 낙관적 락을 통한 좋아요 증가
-    @PostMapping("/{postId}/like")
+    @PostMapping("/{postId}/like/v1")
     public void likePost(@PathVariable Long postId) {
         postWriteService.likePostByOptimisticLock(postId);
+    }
+
+    // 좋아요를 별도 집계 테이블로 나눈 후 좋아요 증가
+    @PostMapping("/{postId}/like/v2")
+    public void likePostV2(@PathVariable Long postId,
+                           @RequestParam Long memberId) {
+        createPostLikeUsecase.execute(postId, memberId);
     }
 }
